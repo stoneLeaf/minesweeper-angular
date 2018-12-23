7e43d8d752f6ae7fbf4e91874fa61503d854cf75
stoneLeaf <owi.stoneleaf@gmail.com>
2018-12-23 17:57:06 +0100

    Use service in start screen component
    Dynamically iterate board sizes and difficulty levels from default
    values set up as static fields.

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,19 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-// TODO: enumerate enums in template instead of hard-coding
-// see https://stackoverflow.com/questions/38554562/how-can-i-use-ngfor-to-iterate-over-typescript-enum-as-an-array-of-strings
-
-enum BoardSize {
-  Small,
-  Medium,
-  Large
-}
-
-enum DifficultyLevel {
-  Beginner,
-  Intermediate,
-  Expert
-}
+import { GameService } from '../game.service';
+import { BoardSize } from '../board-size';
+import { DifficultyLevel } from '../difficulty-level';
 
 @Component({
   selector: 'app-start-screen',
@@ -21,25 +10,11 @@ enum DifficultyLevel {
   styleUrls: ['./start-screen.component.scss']
 })
 export class StartScreenComponent implements OnInit {
-  BoardSize = BoardSize;
-  DifficultyLevel = DifficultyLevel;
+  boardSizes = BoardSize.getSizes();
+  difficultyLevels = DifficultyLevel.getLevels();
 
-  size: BoardSize;
-  difficulty: DifficultyLevel;
+  constructor(private gameService: GameService) { }
 
-  constructor() { }
-
-  ngOnInit() {
-    this.size = BoardSize.Medium;
-    this.difficulty = DifficultyLevel.Beginner;
-  }
-
-  selectSize(size: BoardSize) {
-    this.size = size;
-  }
-
-  selectDifficulty(difficulty: DifficultyLevel) {
-    this.difficulty = difficulty;
-  }
+  ngOnInit() { }
 
 }
