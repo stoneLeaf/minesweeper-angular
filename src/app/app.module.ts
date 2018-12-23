fc076cb109b1668a3bda5b87eee7deab4fc6cd97
stoneLeaf <owi.stoneleaf@gmail.com>
2018-12-23 23:16:28 +0100

    Add footer and update start screen styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
+import { GlobalFooterComponent } from './global-footer/global-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartScreenComponent
+    StartScreenComponent,
+    GlobalFooterComponent
   ],
   imports: [
     BrowserModule,
