2d5cfe2ae6e31ceb831a5d0cc0fbdb55ba617d21
stoneLeaf <owi.stoneleaf@gmail.com>
2018-12-29 15:56:02 +0100

    Improve counter & timer formatting

--- src/app/components/game-top-bar/game-top-bar.component.ts
+++ src/app/components/game-top-bar/game-top-bar.component.ts
@@ -15,12 +15,30 @@ export class GameTopBarComponent {
     this.gameService.newGame();
   }
 
-  numberForDisplay(value: number, length: number): string {
-    const minus = (value < 0) ? '-' : '';
-    let output = '' + Math.abs(value);
-    while ((minus + output).length < length) {
+  /**
+   * Format number for displaying in the top bar counter & timer:
+   * - cap value to max value for length
+   * - add leading zeroes
+   * - put the minus sign in front
+   *
+   * @param value number to be formatted
+   * @param length output length
+   */
+  formatForDisplay(value: number, length: number): string {
+    let prefix = '';
+    if (value < 0) {
+      length--;
+      prefix = '-';
+      value = Math.abs(value);
+    }
+    const max = Math.pow(10, length) - 1;
+    if (value > max) {
+      value = max;
+    }
+    let output = '' + value;
+    while (output.length < length) {
       output = '0' + output;
     }
-    return minus + output;
+    return prefix + output;
   }
 }
