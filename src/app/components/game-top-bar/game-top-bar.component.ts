d44db793f671060a336f67e386f6cb0cd5f7c6fb
stoneLeaf <owi.stoneleaf@gmail.com>
2018-12-30 22:13:24 +0100

    Make game top bar component subscribe to status & uncovering subjects

--- src/app/components/game-top-bar/game-top-bar.component.ts
+++ src/app/components/game-top-bar/game-top-bar.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { GameService } from '../../services/game.service';
+
 
 @Component({
   selector: 'app-game-top-bar',
   templateUrl: './game-top-bar.component.html',
   styleUrls: ['./game-top-bar.component.scss']
 })
-export class GameTopBarComponent {
+export class GameTopBarComponent implements OnInit {
+  gameStatus: string;
+  uncovering = false;
 
   constructor(private gameService: GameService) { }
+
+  ngOnInit() {
+    this.gameService.gameStatus$.subscribe((status) => {
+      this.gameStatus = status;
+    });
+    this.gameService.uncoverings$.subscribe(() => {
+      if (!this.uncovering) {
+        this.uncovering = true;
+        setInterval(() => {
+          this.uncovering = false;
+        }, 250);
+      }
+    });
+  }
 
   newGame() {
     this.gameService.newGame();
